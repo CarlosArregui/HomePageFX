d3b6c28cdfd525fac8c8232dee0e6c967bf99dfc
carre <carre@LAPTOP-LKARUGPA>
2018-12-01 12:22:11 +0100

    HomePage en javaFX

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        FXMLLoader loader =new FXMLLoader(getClass().getResource("HomePage.fxml"));
+        AnchorPane pane=loader.load();
+        Scene scene=new Scene(pane);
+        primaryStage.setTitle("Home page");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
